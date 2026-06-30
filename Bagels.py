6c86f5bc3da6bda379f4a4a604affca4538e66ca
sibefoblessing01-hub <sibefoblessing01@gmail.com>
2026-06-30 14:46:34 -0700

    Add files via upload

---
+++ Bagels.py
@@ -0,0 +1,92 @@
+"""Bagels, by Al Sweigart al@inventwithpython.com
+A deductive logic game where you must guess a number based on clues.
+View this code at https://nostarch.com/big-book-small-python-projects
+A version of this game is featured in the book "Invent Your Own
+Computer Games with Python" https://nostarch.com/inventwithpython
+Tags: short, game, puzzle"""
+
+import random
+
+NUM_DIGITS=3
+MAX_GUESSES=10
+
+
+def main():
+    print('''Bagels, a deductive logic game.
+By Al Sweigart al@inventwithpython.com
+
+I am thinking of a {}-digit number with no repeated digits.
+Try to guess what it is. Here are some clues:
+When I say:       That means:
+Pico                  One digit is correct but in the wrong position.
+Fermi                One digit is correct and in the right position.
+Bagels              No digit is correct.
+
+For example, if the secret number was 248 and your guess was 843, the
+clues would be Fermi Pico.'''.format(NUM_DIGITS))
+
+    while True:
+    
+        secretNum=getSecretNum()
+        print("i have thought up a number.")
+        print("You have {} guesses to get it.".format(MAX_GUESSES))
+    
+        numGuesses=1
+        while numGuesses<=MAX_GUESSES:
+            guess=""
+        
+            while len(guess)!=NUM_DIGITS or not guess.isdecimal():
+                print("Guess #{}:".format(numGuesses))
+                guess=input("> ")  
+    
+            clues=getClues(guess,secretNum)
+            print(clues)
+            numGuesses+=1
+        
+            if guess==secretNum:
+                break
+            if numGuesses>MAX_GUESSES:
+                print("YOU RAN OUT OF  GUESSES")
+                print("The answer is {}.".format(secretNum))
+            
+            
+        print("Do you want to  play again?(yes or no)")
+        if not input("> ").lower().startswith("y"):
+            break
+    print("thanks for playing!")
+
+def getSecretNum():
+        numbers=list("0123456789")      
+        random.shuffle(numbers)
+        secretNum=""
+        for i in range(NUM_DIGITS):
+            secretNum+=str(numbers[i])
+        return secretNum
+
+def getClues(guess,secretNum):
+    if guess==secretNum:
+        return "You got it!"
+    
+    clues=[]
+    
+    for i in range(len(guess)):
+        if guess[i]==secretNum[i]:
+            # A correct digit is in the correct place.
+            clues.append("Fermi")
+        elif guess[i] in secretNum:
+            # A correct digit is in the incorrect place.
+            clues.append("Pico")
+    if len(clues)==0:
+        return  "Bagels"
+    else:
+        # Sort the clues into alphabetical order so their original order
+        # doesn't give information away.
+        clues.sort()
+        # Make a single string from the list of string clues.
+        return "".join(clues)
+    
+# If the program is run (instead of imported), run the game:
+if __name__ == '__main__':
+    main()
+    
+        
